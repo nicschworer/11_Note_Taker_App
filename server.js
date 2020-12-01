3d54fd92fd1ffa0a2d7e477d207258b3a5e0f160
Nic Schworer <nic.schworer@gmail.com>
2020-11-30 20:11:34 -0500

    added compression

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const fs = require('fs');
+const compression = require("compression")
 
-
+app.use(compression());
 
 const app = express();
 const PORT = process.env.PORT || 3000;
